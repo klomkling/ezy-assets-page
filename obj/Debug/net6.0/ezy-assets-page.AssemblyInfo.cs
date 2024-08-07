8ee639164e79904dc08c8ea0fd49cb8f71c9e7f3
Sarawut Phaekuntod <sarawut@idevs.work>
2024-08-08 05:24:00 +0700

    add ruk-com.json

--- obj/Debug/net6.0/ezy-assets-page.AssemblyInfo.cs
+++ obj/Debug/net6.0/ezy-assets-page.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ezy-assets-page")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04eee383315d19be56a1a470f82cb16cef8642ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afb6f1f0debc5b8000121b54afc3e05663962cf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ezy-assets-page")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ezy-assets-page")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
